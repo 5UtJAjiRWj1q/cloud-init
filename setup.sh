f81a513b05a65b8f84aa712eb0fec782aa9d8c6b
Thomas Markus <t.markus@proventis.net>
2020-05-05 07:31:29 +0200

    remove any hetzner part

--- setup.sh
+++ setup.sh
@@ -5,33 +5,16 @@ MYNAME=$(pwd)/$(basename $0)
 
 CONFIGFILE=/etc/proventis/setup.config
 
-command="${1:-update}"
-shift
-
 while [[ $# -gt 0 ]] ; do
   key="$1"
 
   case $key in
-    --hcloud-token)
-        TOKEN="$2"
-        shift
-        shift
-      ;;
-    --whitelisted-ips)
-        WHITELIST_S="$2"
-        shift
-        shift
-      ;;
     --docker)
         DOCKERINSTALL="1"
         shift
       ;;
     --local)
         LOCALINSTALL="1"
-        shift
-      ;;
-    --floating-ips)
-        FLOATING_IPS="--floating-ips"
         shift
       ;;
     *)
@@ -65,56 +48,16 @@ function apt-get() {
   /usr/bin/apt-get "$@"
 }
 
-function updateSystem() {
-  if [ -f "$CONFIGFILE" ]; then
-    source $CONFIGFILE
-  fi
-
-  NEW_NODE_IPS=( $(curl -s -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api.hetzner.cloud/v1/servers' | jq -r '.servers[].public_net.ipv4.ip') )
-
-  touch /etc/current_node_ips
-  cp /etc/current_node_ips /etc/old_node_ips
-  echo "" > /etc/current_node_ips
-
-  for IP in "${NEW_NODE_IPS[@]}"; do
-    ${ufw} allow from "$IP"
-    echo "$IP" >> /etc/current_node_ips
-  done
-
-  IFS=$'\r\n' GLOBIGNORE='*' command eval 'OLD_NODE_IPS=($(cat /etc/old_node_ips))'
-
-  declare -a REMOVED=()
-  for i in "${OLD_NODE_IPS[@]}"; do
-    skip=
-    for j in "${NEW_NODE_IPS[@]}"; do
-      [[ $i == $j ]] && { skip=1; break; }
-    done
-    [[ -n $skip ]] || REMOVED+=("$i")
-  done
-  declare -a REMOVED
-
-  for IP in "${REMOVED[@]}"; do
-    ${ufw} deny from "$IP"
-  done
-
-  FLOATING_IPS=${FLOATING_IPS:-"0"}
-
-  if [ -n "$FLOATING_IPS" ]; then
-    FLOATING_IPS=( $(curl -s -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api.hetzner.cloud/v1/floating_ips' | jq -r '.floating_ips[].ip') )    
-
-    for IP in "${FLOATING_IPS[@]}"; do
-      ip addr add $IP/32 dev eth0
-    done  
-  fi
-}
-
 function installDocker() {
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  apt -yq update
-  apt-get -yq install docker-ce docker-ce-cli containerd.io
-  systemctl enable docker
-  systemctl start docker
+  (
+    source /etc/os-release
+    curl -fsSL https://download.docker.com/linux/${ID}/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${ID} $(lsb_release -cs) stable"
+    apt-get -yq update
+    apt-get -yq install docker-ce docker-ce-cli containerd.io
+    systemctl enable docker
+    systemctl start docker
+  )
 }
 
 function installDefaultSoftware() {
@@ -178,46 +121,11 @@ function installPostfix() {
   postfix reload
 }
 
-function createConfigFile() {
-  mkdir -p $(dirname $CONFIGFILE)
-  cat > $CONFIGFILE << EOF
-TOKEN=${TOKEN}
-WHITELIST_S=${WHITELIST_S}
-FLOATING_IPS=${FLOATING_IPS}
-EOF
-}
-
 function disablePasswordAuthentication() {
   sed -i 's/[#]*PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
   sed -i 's/[#]*PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
   systemctl restart sshd
-}
-
-function setupFirewall() {
-  apt-get install -yq jq ufw fail2ban
-
-  ${ufw} allow proto tcp from any to any port 22,80,443
-  ${ufw} -f enable
-
-  IFS=', ' read -r -a WHITELIST <<< "$WHITELIST_S"
-
-  for IP in "${WHITELIST[@]}"; do
-    ufw allow from "$IP"
-  done
-
-  ${ufw} allow from 10.43.0.0/16
-  ${ufw} allow from 10.42.0.0/16
-
-  ${ufw} -f default deny incoming
-  ${ufw} -f default allow outgoing
-}
-
-function setupCrontab() {
-  crontab -l | {
-    cat
-    echo "* * * * * ${MYNAME}"
-  } | crontab -
 }
 
 function setupSystem() {
@@ -232,30 +140,14 @@ function setupSystem() {
     apt-get -yq update
   fi
   apt-get -yq upgrade
-  apt-get -yq install ca-certificates
+  installDefaultSoftware
+  setupCACertificate
+  installPostfix
 
   if [ -n "$DOCKERINSTALL" ]; then
     installDocker
   fi
 
-  setupFirewall
-  setupCrontab
-  createConfigFile
-  installDefaultSoftware
-  setupCACertificate
-  installPostfix
-  updateSystem
-
 }
 
-case $command in
-  setup)
-    setupSystem
-    ;;
-  update)
-    updateSystem > /dev/null
-    ;;
-  *)
-    echo "unknown command!"
-    ;;
-esac
+setupSystem
